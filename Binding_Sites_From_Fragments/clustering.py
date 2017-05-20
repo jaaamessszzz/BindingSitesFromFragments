a0a726f86f4b4b42025f6d5d1b35775778fd6876
James Lucas <jameslucas@otp584430ots.ucsf.edu>
2017-05-19 22:19:16 -0700

    Outlined what I want clustering to lok like in general. I realized that I need to make some adjustments to the alignments class... namely split into two classes where one inherits from the other. So one base class for each unique ligand, and then a sub class for each pdb.

--- Binding_Sites_From_Fragments/clustering.py
+++ Binding_Sites_From_Fragments/clustering.py
@@ -13,11 +13,12 @@ class Cluster():
     
     """
 
-    def __init__(self, user_defined_dir, current_fragment):
-        self.processed_PDBs_dir = os.path.join(self.user_defined_dir, 'Transformed_Aligned_PDBs', current_fragment)
-        self.pdb_object_list = self.import_pdbs()
+    def __init__(self, user_defined_dir, current_fragment, weights):
+        self.processed_PDBs_dir = os.path.join(user_defined_dir, 'Transformed_Aligned_PDBs', current_fragment)
+        self.weights = weights
+        self.pdb_object_list = self._import_pdbs()
 
-    def import_pdbs(self):
+    def _import_pdbs(self):
         """
         For each fragment ensemble, converts each residue in all processed PDBs into objects with representative 
         matrices and other relevant information
@@ -26,7 +27,9 @@ class Cluster():
         """
         processsed_residue_list = []
         for pdb in pdb_check(self.processed_PDBs_dir):
-            processsed_residue_list += [fragment_PDB(pdb, residue) for residue in prody.parsePDB(pdb)]
+            pdb_info = os.path.basename(os.path.normpath(pdb))
+            prody_pdb = prody.parsePDB(pdb)
+            processsed_residue_list += [fragment_PDB(pdb_info, self.weights, prody_pdb, residue) for residue in prody_pdb]
         return processsed_residue_list
 
     def cluster(self):
@@ -60,21 +63,28 @@ class Cluster():
         #                      )
         #
         # print(np.linalg.norm(first - second))
-
+        pprint.pprint(self.pdb_object_list)
 
 class fragment_PDB():
     """
     Class for holding all information related to a processed fragment PDB
+    
+    :param pdb_info:
+    :param prody_pdb:
+    :param prody_residue:
+    :param pdbid:
+    :param vector:
     """
-    def __init__(self, prody_pdb, prody_residue):
+    def __init__(self, pdb_info, weights, prody_pdb, prody_residue):
         self.prody_pdb = prody_pdb
         self.prody_residue = prody_residue
-        self.pdbid = None
-        self.matrix = self.process_residue_into_matrix()
+        self.pdbid = pdb_info.split('_')[0]
+        self.ligand_resname = pdb_info.split('_')[1]
+        self.vector = self.process_residue_into_vector()
 
-    def process_residue_into_matrix(self):
+    def process_residue_into_vector(self):
         """
-        Converting each residue into a representative matrix
+        Converting each residue into a representative vector
 
         Metrics I care about for each residue:
         * Contact distance
@@ -85,12 +95,46 @@ class fragment_PDB():
         * Position of residue relative to fragment
             * Vector from fragment centroid to {residue centroid | closest residue atom }
         * Backbone or side chain
+        
+         {Angstroms} - X component, Vector from fragment centroid to closest residue atom 
+         {Angstroms} - Y component, Vector from fragment centroid to closest residue atom 
+         {Angstroms} - Z component, Vector from fragment centroid to closest residue atom
+         
+         {Angstroms} - Contact distance  
+         { 0 | 1 }   - Backbone contact  
+         { 0 | 1 }   - Sidechain contact 
 
-        :param pdb: path to processed PDB
+         { 0 | 1 }   - Ligand Polar Contact
+         { 0 | 1 }   - Ligand Non-polar Contact
+         
+         { 0 | 1 }   - Hydrophobic, aliphatic { 0 | 1 }
+         { 0 | 1 }   - Hydrophobic, aromatic  { 0 | 1 }
+         { 0 | 1 }   - Polar                  { 0 | 1 }
+         { 0 | 1 }   - Charged, Acidic        { 0 | 1 }
+         { 0 | 1 }   - Charged, Basic         { 0 | 1 }
+         { 0 | 1 }   - Glycine                { 0 | 1 }
+         { 0 | 1 }   - Proline                { 0 | 1 }
+         { 0 | 1 }   - Backbone carbonyl      { 0 | 1 }
+         { 0 | 1 }   - Backbone amino         { 0 | 1 }
+         { 0 | 1 }   - Backbone C/CA          { 0 | 1 }
+        
         :return: 
         """
         # Contact distance
+        prody_ligand = self.prody_pdb.select('resname {}'.format(self.ligand_resname))
+        ligand_residue_distance_matrix = prody.buildDistMatrix(self.prody_residue, prody_ligand)
 
+        # Find minimum score in matrix
+        row_min_indicies = np.amin(ligand_residue_distance_matrix, axis=0)
+        residue_index = np.argmin(row_min_indicies, axis=0)
+        ligand_index = np.argmin(ligand_residue_distance_matrix, axis=0)
+
+        column_index_low = residue_index
+        row_index_low = ligand_index[column_index_low]
+
+        print("Residue atom index: {}".format(column_index_low))
+        print("Ligand atom index: {}".format(row_index_low))
+        print("Contact distance: {}".format(ligand_residue_distance_matrix.item(row_index_low, column_index_low)))
 
         # Residue characteristics
 
@@ -104,3 +148,5 @@ class fragment_PDB():
 
 
         # Backbone or side chain
+
+        return [1]
