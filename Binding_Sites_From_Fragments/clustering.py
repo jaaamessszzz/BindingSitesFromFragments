9034653f39c68ff67c52ed7716141c83bde0438a
James Lucas <jameslucas@Jamess-MacBook-Pro.local>
2017-05-20 18:48:16 -0700

    Finished start of vector respresentation of individual residues

--- Binding_Sites_From_Fragments/clustering.py
+++ Binding_Sites_From_Fragments/clustering.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 import numpy as np
 import prody
 import sklearn
@@ -13,8 +14,8 @@ class Cluster():
     
     """
 
-    def __init__(self, user_defined_dir, current_fragment, weights):
-        self.processed_PDBs_dir = os.path.join(user_defined_dir, 'Transformed_Aligned_PDBs', current_fragment)
+    def __init__(self, processed_PDBs_dir, weights):
+        self.processed_PDBs_dir = processed_PDBs_dir
         self.weights = weights
         self.pdb_object_list = self._import_pdbs()
 
@@ -28,8 +29,13 @@ class Cluster():
         processsed_residue_list = []
         for pdb in pdb_check(self.processed_PDBs_dir):
             pdb_info = os.path.basename(os.path.normpath(pdb))
-            prody_pdb = prody.parsePDB(pdb)
-            processsed_residue_list += [fragment_PDB(pdb_info, self.weights, prody_pdb, residue) for residue in prody_pdb]
+            prody_protein_hv = prody.parsePDB(pdb).select('protein').getHierView()
+            prody_ligand = prody.parsePDB(pdb).select('resname {}'.format(pdb_info.split('_')[1]))
+
+            # Iterate over residues in contacts and generate representative vector with weights applied
+            for chain in prody_protein_hv:
+                processsed_residue_list += [fragment_PDB(residue, prody_ligand, self.weights) for residue in chain]
+
         return processsed_residue_list
 
     def cluster(self):
@@ -39,30 +45,6 @@ class Cluster():
         
         :return: 
         """
-
-        # for fragment in os.listdir(self.processed_PDBs_dir):
-        #     frag_dir = os.path.join(self.processed_PDBs_dir, fragment)
-        #     for pdb in pdb_check(frag_dir):
-        #         self.process_structure_into_matrix(pdb)
-
-        # first = np.asmatrix(
-        #     [
-        #         [1,0,0],
-        #         [1,0,0],
-        #         [1,0,0],
-        #         [1,0,0]
-        #     ]
-        # )
-        # second = np.asmatrix(
-        #     [
-        #         [0,0,1],
-        #         [0,0,1],
-        #         [1,0,0],
-        #         [1,0,0]
-        #                       ]
-        #                      )
-        #
-        # print(np.linalg.norm(first - second))
         pprint.pprint(self.pdb_object_list)
 
 class fragment_PDB():
@@ -75,11 +57,11 @@ class fragment_PDB():
     :param pdbid:
     :param vector:
     """
-    def __init__(self, pdb_info, weights, prody_pdb, prody_residue):
-        self.prody_pdb = prody_pdb
+    def __init__(self, prody_residue, prody_ligand, weights):
         self.prody_residue = prody_residue
-        self.pdbid = pdb_info.split('_')[0]
-        self.ligand_resname = pdb_info.split('_')[1]
+        self.prody_ligand = prody_ligand
+        self.ligand_center = prody.calcCenter(prody_ligand)
+        self.weights = weights
         self.vector = self.process_residue_into_vector()
 
     def process_residue_into_vector(self):
@@ -101,52 +83,97 @@ class fragment_PDB():
          {Angstroms} - Z component, Vector from fragment centroid to closest residue atom
          
          {Angstroms} - Contact distance  
-         { 0 | 1 }   - Backbone contact  
-         { 0 | 1 }   - Sidechain contact 
-
-         { 0 | 1 }   - Ligand Polar Contact
-         { 0 | 1 }   - Ligand Non-polar Contact
+         { 0 | 1 }   - Backbone contact OR Sidechain contact
+         { 0 | 1 }   - Ligand Polar Contact OR Ligand Non-polar Contact
          
-         { 0 | 1 }   - Hydrophobic, aliphatic { 0 | 1 }
-         { 0 | 1 }   - Hydrophobic, aromatic  { 0 | 1 }
-         { 0 | 1 }   - Polar                  { 0 | 1 }
-         { 0 | 1 }   - Charged, Acidic        { 0 | 1 }
-         { 0 | 1 }   - Charged, Basic         { 0 | 1 }
-         { 0 | 1 }   - Glycine                { 0 | 1 }
-         { 0 | 1 }   - Proline                { 0 | 1 }
-         { 0 | 1 }   - Backbone carbonyl      { 0 | 1 }
-         { 0 | 1 }   - Backbone amino         { 0 | 1 }
-         { 0 | 1 }   - Backbone C/CA          { 0 | 1 }
+         { 0 | 1 }   - Side chain has hydrogen bond donor/acceptor (DEHKNQRSTY)
+         
+         { 0 | 1 }   - Hydrophobic, aliphatic (AILV)
+         { 0 | 1 }   - Hydrophobic, aromatic (FWY)
+         { 0 | 1 }   - Polar (NCQMST)
+         { 0 | 1 }   - Charged, Acidic (DE)
+         { 0 | 1 }   - Charged, Basic (HKR)
+         { 0 | 1 }   - Glycine
+         { 0 | 1 }   - Proline
+         { 0 | 1 }   - Backbone carbonyl
+         { 0 | 1 }   - Backbone amino
+         { 0 | 1 }   - Backbone C/CA
         
         :return: 
         """
-        # Contact distance
-        prody_ligand = self.prody_pdb.select('resname {}'.format(self.ligand_resname))
-        ligand_residue_distance_matrix = prody.buildDistMatrix(self.prody_residue, prody_ligand)
+        ligand_residue_distance_matrix = prody.buildDistMatrix(self.prody_residue, self.prody_ligand)
 
         # Find minimum score in matrix
         row_min_indicies = np.amin(ligand_residue_distance_matrix, axis=0)
-        residue_index = np.argmin(row_min_indicies, axis=0)
-        ligand_index = np.argmin(ligand_residue_distance_matrix, axis=0)
+        ligand_index = np.argmin(row_min_indicies, axis=0)
+        residue_index = np.argmin(ligand_residue_distance_matrix, axis=0)
 
-        column_index_low = residue_index
-        row_index_low = ligand_index[column_index_low]
+        column_index_low = ligand_index
+        row_index_low = residue_index[column_index_low]
 
-        print("Residue atom index: {}".format(column_index_low))
-        print("Ligand atom index: {}".format(row_index_low))
-        print("Contact distance: {}".format(ligand_residue_distance_matrix.item(row_index_low, column_index_low)))
+        # Contact distance
+        min_contact_distance = ligand_residue_distance_matrix.item(row_index_low, column_index_low)
+
+        residue_base_index = self.prody_residue.getIndices()[0]
+        ligand_base_index = self.prody_ligand.getIndices()[0]
+
+        residue_contact_atom = self.prody_residue.select('index {}'.format(residue_base_index + row_index_low))
+        ligand_contact_atom = self.prody_ligand.select('index {}'.format(ligand_base_index + column_index_low))
+
+        # Residue Contact Type
+        residue_contact_type = 0 if residue_contact_atom.getNames()[0] in ['C', 'CA', 'N', 'O'] else 1
+
+        # Ligand Contact Type
+        ligand_contact_type = 1 if ligand_contact_atom.getNames()[0][0] in ['C'] else 0
+
+        # Vector from fragment centroid to closest residue atom
+        vector = (residue_contact_atom.getCoords() - self.ligand_center)[0]
+
+        # Side chain has hydrogen bond donor/acceptor (DEHKNQRSTY)
+        h_bond_donor_acceptor = 1 if residue_contact_atom.getResnames()[0] in ['ASP', 'GLU', 'HIS', 'LYS', 'ASN', 'GLN', 'ARG', 'SER', 'THR', 'TYR'] else 0
 
         # Residue characteristics
+        # {0 | 1} - Hydrophobic, aliphatic(AILV)
+        greasy_ali = 1 if residue_contact_atom.getResnames()[0] in ['ALA', 'ILE', 'LEU', 'VAL'] else 0
+        # {0 | 1} - Hydrophobic, aromatic(FWY)
+        greasy_aro = 1 if residue_contact_atom.getResnames()[0] in ['PHE', 'TYR', 'TRP'] else 0
+        # {0 | 1} - Polar(NCQMST)
+        polar = 1 if residue_contact_atom.getResnames()[0] in ['ASN', 'CYS', 'GLN', 'MET', 'SER', 'THR'] else 0
+        # {0 | 1} - Charged, Acidic(DE)
+        charged_acid = 1 if residue_contact_atom.getResnames()[0] in ['ASP', 'GLU'] else 0
+        # {0 | 1} - Charged, Basic(HKR)
+        charged_basic = 1 if residue_contact_atom.getResnames()[0] in ['HIS', 'LYS', 'ARG'] else 0
+        # {0 | 1} - Glycine
+        glycine = 1 if residue_contact_atom.getResnames()[0] in ['GLY'] else 0
+        # {0 | 1} - Proline
+        proline = 1 if residue_contact_atom.getResnames()[0] in ['PRO'] else 0
+        # {0 | 1} - Backbone carbonyl
+        bb_carbonyl = 1 if residue_contact_atom.getNames()[0] in ['O'] else 0
+        # {0 | 1} - Backbone amino
+        bb_amino = 1 if residue_contact_atom.getNames()[0] in ['N'] else 0
+        # {0 | 1} - Backbone C / CA
+        bb_c_ca = 1 if residue_contact_atom.getNames()[0] in ['C', 'CA'] else 0
 
+        residue_vector = [
+            vector[0],
+            vector[1],
+            vector[2],
+            min_contact_distance,
+            residue_contact_type,
+            ligand_contact_type,
+            h_bond_donor_acceptor,
+            greasy_ali,
+            greasy_aro,
+            polar,
+            charged_acid,
+            charged_basic,
+            glycine,
+            proline,
+            bb_carbonyl,
+            bb_amino,
+            bb_c_ca
+        ]
 
-        # Amino acid identity or degenerate amino acid groups?
-        # Amino acid chemical characteristics e.g. [Sigma Amino Acid Reference Chart]
-        #       (http://www.sigmaaldrich.com/life-science/metabolomics/learning-center/amino-acid-reference-chart.html)
+        print(residue_vector)
 
-
-        # Vector from fragment centroid to {residue centroid | closest residue atom }
-
-
-        # Backbone or side chain
-
-        return [1]
+        return residue_vector
