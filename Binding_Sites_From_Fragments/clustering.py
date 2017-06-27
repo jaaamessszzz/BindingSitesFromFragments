c3c3f768c3f679e48c11765102f8df5f7f18d829
James Lucas <jameslucas@jamess-mbp.attlocal.net>
2017-06-26 19:14:15 -0700

    Fixed ligand atom selection for free amino acid ligands

--- Binding_Sites_From_Fragments/clustering.py
+++ Binding_Sites_From_Fragments/clustering.py
@@ -30,7 +30,6 @@ class Cluster():
         """
         processsed_residue_list = []
         for pdb in pdb_check(self.processed_PDBs_dir):
-            # todo: somehow get original filename out of prody_residue
             pdb_info = os.path.basename(os.path.normpath(pdb))
             prody_protein = prody.parsePDB(pdb).select('protein')
             # Check that residues exist within cutoff distance provided in alignments, otherwise pass
@@ -39,11 +38,10 @@ class Cluster():
             else:
                 prody_protein_hv = prody_protein.getHierView()
 
-            prody_ligand = prody.parsePDB(pdb).select('resname {}'.format(pdb_info.split('_')[1]))
+            prody_ligand = prody.parsePDB(pdb).select('hetatm and resname {}'.format(pdb_info.split('_')[1]))
 
             # Iterate over residues in contacts and generate representative vector with weights applied
             for chain in prody_protein_hv:
-
                 processsed_residue_list += [fragment_PDB(residue, pdb_info, prody_ligand, self.distance_cutoff, self.weights) for residue in chain]
 
         processsed_residue_list_cleaned = [residue for residue in processsed_residue_list if residue.vector is not None]
@@ -117,8 +115,6 @@ class Cluster():
             df = df.append(self.generate_report_row(residue_list, cluster))
 
             # Output residue PDBs for each cluster
-            # todo: add all residues to a single object and export that... the current onslaught of individual PDBs is way too intense
-            # todo: somehow get original filename out of prody_residue
             count=0
             for residue in residue_list:
                 prody.writePDB(os.path.join(fragment_cluster_path, 'Cluster_{0}-Residue_{1}-{2}.pdb'.format(cluster, str(count), residue[1].pdb_info)), residue[1].prody_residue)
