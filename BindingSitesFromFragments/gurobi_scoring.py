b33f5b2f20c5b9d8238b285e300fa9f1eb5352e7
James Lucas <jameslucas@otp584378ots.ucsf.edu>
2017-10-02 13:59:30 -0700

    Floor interaction energies -0.1 >= x > 0 to 0, set pairwise residue constraints to only consider energies > 0

--- BindingSitesFromFragments/gurobi_scoring.py
+++ BindingSitesFromFragments/gurobi_scoring.py
@@ -89,6 +89,7 @@ class score_with_gurobi():
             SELECT struct_id, resNum1, resNum2, 
             CASE
             WHEN round(sum(score_value), 4) >= 0 THEN 1
+            WHEN round(sum(score_valeu), 4) >= -0.1 THEN 0
             ELSE round(sum(score_value), 4)
             END 
             as score_total from relevant_2b_scores group by struct_id, resNum1, resNum2;
@@ -110,7 +111,7 @@ class score_with_gurobi():
             # Only add residues to Model if ligand-residue interaction energy is less than X
             ligand_residue_scores = score_table.groupby(['struct_id', 'resNum1']).get_group((struct_id, 1))
             for index, row in ligand_residue_scores.iterrows():
-                if row['score_total'] <= -0.6:
+                if row['score_total'] <= -0.75:
                     MIP_var_dict[row['resNum2']] = residue_interactions.addVar(vtype=GRB.BINARY, name=str(row['resNum2']))
 
             # List of residue indcies used in Model
@@ -141,7 +142,7 @@ class score_with_gurobi():
             # Residues cannot be a solution if two-body interaction energy is above X
             current_struct_scores = score_table.groupby(['struct_id']).get_group(struct_id)
             for index, row in current_struct_scores.iterrows():
-                if row['score_total'] >= -0.1 and all([row['resNum1'] in MIP_residx_list, row['resNum2'] in MIP_residx_list]):
+                if row['score_total'] > 0 and all([row['resNum1'] in MIP_residx_list, row['resNum2'] in MIP_residx_list]):
                     residue_interactions.addConstr(MIP_var_dict[int(row['resNum1'])] + MIP_var_dict[int(row['resNum2'])] <= 1)
 
             # Set Parameters
