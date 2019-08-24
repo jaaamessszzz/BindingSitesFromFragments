ce30e4ec5a381ac772289a9bb88ba1bbe8e44b73
jaaamessszzz <james.lucas@berkeley.edu>
2019-08-24 15:44:02 -0700

    No more need for processed_check()

--- BindingSitesFromFragments/utils.py
+++ BindingSitesFromFragments/utils.py
@@ -35,15 +35,6 @@ def pdb_check(dir, base_only=False, conformer_check=False, extension='.pdb'):
                     yield path
 
 
-def processed_check(processed_dir, pdb, rejected_list):
-    pdbid = pdb.split('.')[0].upper()
-
-    in_reject_list = any([pdb == reject for reject in rejected_list])
-    # already_processed = os.path.exists(os.path.join(processed_dir, '{}_processed.pdb'.format(pdbid)))
-    already_processed = any([pdbid == file[:4].upper() for file in os.listdir(processed_dir)])
-
-    return any([in_reject_list, already_processed])
-
 # --- Common manipulations --- #
 
 def minimum_contact_distance(a_H, b_H, return_indices=False, strip_H=True):
